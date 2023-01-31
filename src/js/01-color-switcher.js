fb7770fbe36f628e93d5b5e15fe29de6b4fa640a
Alex Pan <alxtouch@gmail.com>
2023-01-31 09:33:11 +0200

    add task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const btnStart = document.querySelector('button[data-start]')
+const btnStop = document.querySelector('button[data-stop]')
+const body = document.querySelector('body');
 
+btnStart.disabled = false;
+btnStop.disabled = true;
+
+let timerId = null;
+const INTERVAL_DELAY = 1000;
+
+btnStart.addEventListener('click', onBtnStartClick);
+btnStop.addEventListener('click', onBtnStopClick);
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+  function changeBackground() {
+    body.style.backgroundColor = getRandomHexColor();
+  }
+
+  function onBtnStartClick() {
+    timerId = setInterval(changeBackground, INTERVAL_DELAY);
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+  }
+
+  function onBtnStopClick() {
+    
+    btnStart.disabled = false;
+    btnStop.disabled = true;
+    clearInterval(timerId);
+  }
